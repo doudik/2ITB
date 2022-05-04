621f00655bb199df18db885a43d0c32adcfa5fa3
Miroslav Douda <miramirinda@outlook.cz>
2022-05-04 09:43:00 +0200

    Zadani Pivovar

---
+++ Pivovar/Pivovar_2ITB/Pivovar_2ITB/Clovek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pivovar_2ITB
+{
+    internal class Clovek
+    {
+        Random rnd = new Random();
+        string jmeno; //jméno
+        int vek; //jeho věk
+        int zustatek; //počet peněz
+        int pocetPiv; //kolik vypil piv
+        float naladaCislo; //Range 0-100 pro náladu
+        bool nalada; //Dobrá - špatná
+
+        public Clovek(string jmeno, int vek) {
+            this.jmeno = jmeno;
+            this.vek = vek;
+            zustatek = rnd.Next(200, 1500);
+            pocetPiv = 0;
+            naladaCislo = rnd.Next(0, 100);
+            if (naladaCislo <  50)
+            {
+                nalada = false;
+            }
+            else {
+                nalada = true;
+            }
+        }
+    }
+}
---
+++ Pivovar/Pivovar_2ITB/Pivovar_2ITB/Hospoda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pivovar_2ITB
+{
+    internal class Hospoda
+    {
+    }
+}
